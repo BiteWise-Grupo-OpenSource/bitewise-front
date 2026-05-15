3d2bfa281ae336be2f75f3f4e89ed9afca42921e
JvnnDev <u202318220@upc.edu.pe>
2026-05-14 21:18:00 -0500

    feat-setup-Angular-Material-i18n-json-server-brand-DNA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { LoginPageComponent } from './features/usuarios/presentation/pages/login/login-page.component';
+import { RegisterPageComponent } from './features/usuarios/presentation/pages/register/register-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  {
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+      {
+        path: 'usuarios',
+        loadChildren: () =>
+          import('./features/usuarios/usuarios.routes').then(m => m.USUARIOS_ROUTES)
+      }
+    ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
